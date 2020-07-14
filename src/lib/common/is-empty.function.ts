b30cfed6eff828aa0b85c86fafbae66ddf3a9bfc
Fedor Usakov <fedor.usakov@meistersoft.ru>
2020-07-14 10:27:58 +0300

    build(inital) setup initial build scripts & configs

---
+++ src/lib/common/is-empty.function.ts
@@ -0,0 +1,42 @@
+import { isNullOrUndefined } from './is-null-or-undefined.function';
+
+export function isEmpty(input: null | undefined): true;
+export function isEmpty(input: NonNullable<number | boolean | symbol>): false;
+export function isEmpty(input: string): input is "";
+export function isEmpty<T>(input: T[]): input is [];
+export function isEmpty(input: object): input is {};
+export function isEmpty<T>(input: T): boolean {
+  if (isNullOrUndefined(input)) {
+    return true;
+  }
+  if (isObject(input) && Array.isArray(input)) {
+    return isEmptyArray(input);
+  }
+  if (isObject(input)) {
+    return isEmptyObject(input);
+  }
+  if (isString(input)) {
+    return isEmptyString(input);
+  }
+  return false;
+}
+
+const isEmptyArray = <T>(input: T[]): input is [] => {
+  return Array.isArray(input) && Object.is(input.length, 0);
+};
+
+const isEmptyObject = (input: object): input is {} => {
+  return isEmptyArray(Object.keys(input));
+};
+
+const isEmptyString = (input: string): input is "" => {
+  return Object.is(input.length, 0);
+};
+
+const isObject = (input: unknown): input is object => {
+  return typeof input === "object";
+};
+
+const isString = (input: unknown): input is string => {
+  return typeof input === "string";
+};
---
+++ src/lib/filesystem/copy-folder-with-files.function.ts
@@ -0,0 +1,40 @@
+import { copyFile, existsSync, mkdirSync, readdirSync, statSync } from 'fs';
+import * as path from 'path';
+
+import { isNullOrUndefined } from '../common/is-null-or-undefined.function';
+import { Nullable } from './../../internal/types/nullable.type';
+
+interface Options {
+  onCopy?: Nullable<(sourcePath?: string, targetPath?: string) => void>;
+}
+
+export function copyFolderWithFiles(
+  sourcePath: string,
+  targetPath: string,
+  options: Nullable<Options> = null
+): void {
+  const { onCopy }: Options = isNullOrUndefined(options)
+    ? { onCopy: null }
+    : options;
+  const sourceIsDirectory: boolean =
+    existsSync(sourcePath) && statSync(sourcePath).isDirectory();
+  const targetDirectoryExists: boolean = existsSync(targetPath);
+  if (sourceIsDirectory && !targetDirectoryExists) {
+    mkdirSync(targetPath);
+  }
+  if (sourceIsDirectory) {
+    readdirSync(sourcePath).forEach((filePath: string) => {
+      copyFolderWithFiles(
+        path.join(sourcePath, filePath),
+        path.join(targetPath, filePath),
+        options
+      );
+    });
+    return;
+  }
+  copyFile(sourcePath, targetPath, () => {
+    if (!isNullOrUndefined(onCopy) && typeof onCopy === "function") {
+      onCopy(sourcePath, targetPath);
+    }
+  });
+}
