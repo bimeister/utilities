f871c8831dbacfa6e768d226b05efc445f8bb1cf
Fedor Usakov <fedor.usakov@meistersoft.ru>
2020-08-10 17:44:04 +0300

    feature(error logging via tapLog())

--- src/lib/rxjs/operators/tap-log.operator.ts
+++ src/lib/rxjs/operators/tap-log.operator.ts
@@ -4,15 +4,20 @@ import { tap } from 'rxjs/operators';
 export const tapLog = <T>(...prefixes: unknown[]): MonoTypeOperatorFunction<T> => (
   source$: Observable<T>
 ): Observable<T> => {
-  const { group, log, groupCollapsed, trace, groupEnd }: Console = console;
   return source$.pipe(
-    tap((data: T) => {
-      group('tapLog');
-      log(...prefixes, data);
-      groupCollapsed('trace');
-      trace();
-      groupEnd();
-      groupEnd();
-    })
+    tap(
+      (data: T) => writeToConsole(data, prefixes, 'default'),
+      (data: Error) => writeToConsole(data, prefixes, 'error')
+    )
   );
 };
+
+function writeToConsole<T>(data: T, prefixes: unknown[], logger: 'error' | 'default' = 'default'): void {
+  const { group, log, groupCollapsed, trace, groupEnd, error }: Console = console;
+  group('tapLog');
+  logger === 'default' ? log(...prefixes, data) : error(...prefixes, data);
+  groupCollapsed('trace');
+  trace();
+  groupEnd();
+  groupEnd();
+}
