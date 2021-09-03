69a2d38e72ff8c84d57b4ed8af30eabc7325a2de
edikdeisling <edikdeisling@gmail.com>
2021-09-03 12:47:29 +0300

    feature: BIM-14755 nav resize observer

---
+++ src/lib/rxjs/functions/resize-observable.function.ts
@@ -0,0 +1,11 @@
+import { Observable, Subscriber } from 'rxjs';
+
+export function resizeObservable(element: Element, options?: ResizeObserverOptions): Observable<ResizeObserverEntry[]> {
+  return new Observable<ResizeObserverEntry[]>((subscriber: Subscriber<ResizeObserverEntry[]>) => {
+    const resizeObserver: ResizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) =>
+      subscriber.next(entries)
+    );
+    resizeObserver.observe(element, options);
+    return () => resizeObserver.unobserve(element);
+  });
+}
