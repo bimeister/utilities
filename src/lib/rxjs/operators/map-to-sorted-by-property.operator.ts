e4afd3c7be5f5a19b5c1da2efb01e56a69496f13
ScarletFlash <scarletflash.dev@gmail.com>
2021-09-21 18:02:03 +0300

    feature: upgrade dependencies

--- src/lib/rxjs/operators/map-to-sorted-by-property.operator.ts
+++ src/lib/rxjs/operators/map-to-sorted-by-property.operator.ts
@@ -3,9 +3,8 @@ import { map } from 'rxjs/operators';
 import type { SortType } from './../../../internal/types/sort-type.type';
 import { sortByProperty } from './../../common/sort-by-property.function';
 
-export const mapToSortedByProperty = <T>(
-  key: keyof T,
-  sortType: SortType = 'ascending'
-): MonoTypeOperatorFunction<T[]> => (source$: Observable<T[]>): Observable<T[]> => {
-  return source$.pipe(map((value: T[]) => sortByProperty(value, key, sortType)));
-};
+export const mapToSortedByProperty =
+  <T>(key: keyof T, sortType: SortType = 'ascending'): MonoTypeOperatorFunction<T[]> =>
+  (source$: Observable<T[]>): Observable<T[]> => {
+    return source$.pipe(map((value: T[]) => sortByProperty(value, key, sortType)));
+  };
