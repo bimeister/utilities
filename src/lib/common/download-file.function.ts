d8b01e85cb2a213d9a157900be6cd7742e559b9d
ScarletFlash <scarletflash.dev@gmail.com>
2021-10-12 12:09:59 +0000

    feature: ReadFile, FileToString functions

--- src/lib/common/download-file.function.ts
+++ src/lib/common/download-file.function.ts
@@ -1,6 +1,8 @@
-export function downloadFile(filename: string, blob: Blob): void {
+export function downloadFile(filename: string, data: Blob | string): void {
+  const dataBlob: Blob = data instanceof Blob ? data : new Blob([data]);
+
   const anchor: HTMLAnchorElement = document.createElement('a');
-  anchor.href = URL.createObjectURL(blob);
+  anchor.href = URL.createObjectURL(dataBlob);
   anchor.download = filename;
   anchor.click();
 }
