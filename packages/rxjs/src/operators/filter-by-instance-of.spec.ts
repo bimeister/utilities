89dd14d7983fd2ba588af3656dedd166d0760be6
Denis Platonov <denis.platonov@bimeister.com>
2023-02-21 08:52:25 +0300

    feature: add new tests for filterByInstanceOf rxjs operator

--- packages/rxjs/src/operators/filter-by-instance-of.spec.ts
+++ packages/rxjs/src/operators/filter-by-instance-of.spec.ts
@@ -1,23 +1,72 @@
 import { from, Observable } from 'rxjs';
 import { filterByInstanceOf } from './filter-by-instance-of.operator';
 
-class SomeClass {
-  public readonly name: string = 'Some name';
+class SomeClassA {
+  public readonly name: string = 'Some name A';
+}
+
+class SomeClassB {
+  public readonly name: string = 'Some name B';
 }
 
 describe('filter-by-instance-of.operator.ts', () => {
-  it('should emit valid value only once', () => {
-    const input$: Observable<unknown> = from([1, 'string', { name: 'Some name' }, new SomeClass()]);
+  it('should emit valid value only once for single type', () => {
+    const input$: Observable<unknown> = from([1, 'string', { name: 'Some name' }, new SomeClassA()]);
 
     const emits: unknown[] = [];
 
     input$
-      .pipe(filterByInstanceOf(SomeClass))
-      .subscribe((output: SomeClass): void => {
+      .pipe(filterByInstanceOf(SomeClassA))
+      .subscribe((output: SomeClassA): void => {
         emits.push(output);
       })
       .unsubscribe();
 
-    expect(emits).toEqual([new SomeClass()]);
+    expect(emits).toEqual([new SomeClassA()]);
+  });
+
+  it('should emit valid value only once for multiple types', () => {
+    const input$: Observable<unknown> = from([1, 'string', { name: 'Some name' }, new SomeClassA(), new SomeClassB()]);
+
+    const emits: unknown[] = [];
+
+    input$
+      .pipe(filterByInstanceOf([SomeClassA, SomeClassB]))
+      .subscribe((output: SomeClassA | SomeClassB): void => {
+        emits.push(output);
+      })
+      .unsubscribe();
+
+    expect(emits).toEqual([new SomeClassA(), new SomeClassB()]);
+  });
+
+  it('should not emit any value for single type', () => {
+    const input$: Observable<unknown> = from([1, 'string', { name: 'Some name' }, new SomeClassB()]);
+
+    const emits: unknown[] = [];
+
+    input$
+      .pipe(filterByInstanceOf(SomeClassA))
+      .subscribe((output: SomeClassA): void => {
+        emits.push(output);
+      })
+      .unsubscribe();
+
+    expect(emits).toEqual([]);
+  });
+
+  it('should not emit any value for multiple types', () => {
+    const input$: Observable<unknown> = from([1, 'string', { name: 'Some name' }, new SomeClassB()]);
+
+    const emits: unknown[] = [];
+
+    input$
+      .pipe(filterByInstanceOf([SomeClassA, SomeClassB]))
+      .subscribe((output: SomeClassA | SomeClassB): void => {
+        emits.push(output);
+      })
+      .unsubscribe();
+
+    expect(emits).toEqual([new SomeClassB()]);
   });
 });
