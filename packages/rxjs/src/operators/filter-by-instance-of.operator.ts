c2dc7013659aaee4c247e64d2630b4576f2c572a
Denis Platonov <denis.platonov@bimeister.com>
2023-02-21 08:51:45 +0300

    feature: refactored filterByInstanceOf rxjs operator so it can takes array of types as an argument

--- packages/rxjs/src/operators/filter-by-instance-of.operator.ts
+++ packages/rxjs/src/operators/filter-by-instance-of.operator.ts
@@ -2,6 +2,19 @@ import type { Constructor } from 'packages/types/src/constructor.type';
 import type { Observable, OperatorFunction } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
-export function filterByInstanceOf<I>(type: Constructor<I>): OperatorFunction<unknown, I> {
-  return (source$: Observable<unknown>) => source$.pipe(filter((value: unknown): value is I => value instanceof type));
+export function filterByInstanceOf<I>(typeOrTypes: Constructor<I> | Constructor<I>[]): OperatorFunction<unknown, I> {
+  const instanceOfTypes: ((value: unknown) => value is I)[] = Array.isArray(typeOrTypes)
+    ? typeOrTypes.map(
+        (type: Constructor<I>): ((value: unknown) => value is I) =>
+          (value: unknown): value is I =>
+            value instanceof type
+      )
+    : [(value: unknown): value is I => value instanceof typeOrTypes];
+
+  return (source$: Observable<unknown>) =>
+    source$.pipe(
+      filter((value: unknown): value is I =>
+        instanceOfTypes.some((instanceOf: (value: unknown) => value is I) => instanceOf(value))
+      )
+    );
 }
