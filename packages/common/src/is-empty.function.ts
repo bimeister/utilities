788e4a8894477196f7899e1e5d8f3f71fc72c90a
Denis Platonov <denis.platonov@bimeister.com>
2023-04-20 20:23:17 +0300

    fix: fixed isEmpty function incorrect logic

--- packages/common/src/is-empty.function.ts
+++ packages/common/src/is-empty.function.ts
@@ -4,38 +4,50 @@
  */
 import { isNil } from './is-nil.function';
 
-export function isEmpty(input: null | undefined): true;
-export function isEmpty(input: NonNullable<number | boolean | symbol>): false;
-export function isEmpty(input: string): input is '';
-export function isEmpty<T>(input: T[]): input is [];
-export function isEmpty(input: object): input is {};
-export function isEmpty(input: unknown): boolean;
 export function isEmpty(input: unknown): boolean {
   if (isNil(input)) {
     return true;
   }
-  if (isObject(input) && Array.isArray(input)) {
-    return isEmptyArray(input);
-  }
+
   if (isObject(input)) {
-    return isEmptyObject(input);
+    if (Array.isArray(input)) {
+      return isEmptyArray(input);
+    }
+
+    if (input instanceof Set || input instanceof Map) {
+      return isEmptySetOrMap(input);
+    }
+
+    if (!(input instanceof Date)) {
+      return isEmptyObject(input);
+    }
   }
+
   if (isString(input)) {
     return isEmptyString(input);
   }
+
+  if (isSymbol(input)) {
+    return false;
+  }
+
   return false;
 }
 
-function isEmptyArray<T>(input: T[]): input is [] {
+function isEmptyArray<T>(input: T[]): boolean {
   return Array.isArray(input) && Object.is(input.length, 0);
 }
 
-function isEmptyObject(input: object): input is {} {
+function isEmptyObject(input: object): boolean {
   return isEmptyArray(Object.keys(input));
 }
 
-function isEmptyString(input: string): input is '' {
+function isEmptyString(input: string): boolean {
   return Object.is(input.length, 0);
+}
+
+function isEmptySetOrMap(input: Set<unknown> | Map<unknown, unknown>): boolean {
+  return input.size === 0;
 }
 
 function isObject(input: unknown): input is object {
@@ -44,4 +56,8 @@ function isObject(input: unknown): input is object {
 
 function isString(input: unknown): input is string {
   return typeof input === 'string';
+}
+
+function isSymbol(input: unknown): input is symbol {
+  return typeof input === 'symbol';
 }
