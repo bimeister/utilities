0af388be225b671d844409959a014b4bd724c900
= <=>
2024-02-07 12:16:33 +0300

    chore: update isUuid type guard

--- packages/common/src/is-uuid.function.ts
+++ packages/common/src/is-uuid.function.ts
@@ -1,6 +1,6 @@
 import type { Uuid } from '@bimeister/utilities.types';
 import { validate } from 'uuid';
 
-export function isUuid(input: string): input is Uuid {
-  return validate(input);
+export function isUuid(input: unknown): input is Uuid {
+  return typeof input === 'string' && validate(input);
 }
