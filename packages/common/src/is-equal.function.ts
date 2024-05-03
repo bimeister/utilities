05c78386017a7f2bc317cb0051871290bec19ef3
= <=>
2024-05-03 09:12:43 +0300

    chore: updated isEqual function to support sets and maps

--- packages/common/src/is-equal.function.ts
+++ packages/common/src/is-equal.function.ts
@@ -16,26 +16,60 @@ export function isEqual(a: unknown, b: unknown): boolean {
   }
 
   if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length !== b.length) {
-      return false;
-    }
+    return areArraysEqual(a, b);
+  }
 
-    return a.every((item: unknown, index: number) => {
-      if (isNil(item) && isNil(b[index])) {
-        return true;
-      }
+  if (a instanceof Set && b instanceof Set) {
+    return areSetsEqual(a, b);
+  }
 
-      if (isString(item) && isString(b[index])) {
-        return item === b[index];
-      }
-
-      if (isNumber(item) && isNumber(b[index])) {
-        return item === b[index];
-      }
-
-      return JSON.stringify(item) === JSON.stringify(b[index]);
-    });
+  if (a instanceof Map && b instanceof Map) {
+    return areMapsEqual(a, b);
   }
 
   return JSON.stringify(a) === JSON.stringify(b);
+}
+
+function areArraysEqual<T>(a: T[], b: T[]): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+
+  return a.every((item: T, index: number) => {
+    if (isNil(item) && isNil(b[index])) {
+      return true;
+    }
+
+    if (isString(item) && isString(b[index])) {
+      return item === b[index];
+    }
+
+    if (isNumber(item) && isNumber(b[index])) {
+      return item === b[index];
+    }
+
+    return JSON.stringify(item) === JSON.stringify(b[index]);
+  });
+}
+
+function areSetsEqual<T>(a: Set<T>, b: Set<T>): boolean {
+  if (a.size !== b.size) {
+    return false;
+  }
+
+  return [...a].every((item: T) => b.has(item));
+}
+
+function areMapsEqual<T, K>(a: Map<T, K>, b: Map<T, K>): boolean {
+  if (a.size !== b.size) {
+    return false;
+  }
+
+  for (const [key, value] of a) {
+    if (!b.has(key) || !isEqual(value, b.get(key))) {
+      return false;
+    }
+  }
+
+  return true;
 }
