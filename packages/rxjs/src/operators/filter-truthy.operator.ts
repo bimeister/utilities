7e5aa1fa1ba36da6d6ebc753e758b61fc6b35e31
= <=>
2024-08-21 07:15:19 +0300

    feature: improve typing and logic for filterTruthy and filterFalsy operators

--- packages/rxjs/src/operators/filter-truthy.operator.ts
+++ packages/rxjs/src/operators/filter-truthy.operator.ts
@@ -1,19 +1,19 @@
-import type { MonoTypeOperatorFunction, Observable } from 'rxjs';
+import type { Observable, OperatorFunction } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
 /**
- * Filters out falsy values from the source observable.
+ * Filters out falsy values (`false`, `0`, `''`, `null`, `undefined`, `NaN`) from the source observable.
  *
  * @template T - The type of elements emitted by the source observable.
- * @returns - An operator that filters out falsy values from the source observable.
+ * @returns An operator that filters out falsy values from the source observable.
  * @example
- *  const input$: Observable<unknown> = from([1, 'string', false, true]);
-
-    input$
-      .pipe(filterTruthy())
-      .subscribe((output: unknown) => { ... })
+ * const input$: Observable<unknown> = from([1, 'string', false, true]);
+ *
+ * input$
+ *   .pipe(filterTruthy())
+ *   .subscribe((output: unknown) => { ... });
  */
-export const filterTruthy: <T>() => MonoTypeOperatorFunction<T> =
+export const filterTruthy: <T>() => OperatorFunction<T, NonNullable<T>> =
   <T>() =>
-  (source: Observable<T>): Observable<T> =>
-    source.pipe(filter<T>((value: T) => Boolean(value)));
+  (source: Observable<T>): Observable<NonNullable<T>> =>
+    source.pipe(filter((value: T): value is NonNullable<T> => Boolean(value)));
