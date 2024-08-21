5d42341d0645edeb5e6b3d7581328662cd36fd0b
= <=>
2024-08-21 12:49:52 +0300

    feature: updated `downloadFile` function and unit tests to cover new functionality

--- packages/common/src/download-file.function.ts
+++ packages/common/src/download-file.function.ts
@@ -1,8 +1,49 @@
-export function downloadFile(filename: string, data: Blob | string): void {
-  const dataBlob: Blob = data instanceof Blob ? data : new Blob([data]);
+/* eslint-disable @typescript-eslint/unified-signatures */
+import { isNil } from './is-nil.function';
 
+/**
+ * Initiates a file download using a pre-signed URL.
+ *
+ * @param presignedUrl - The pre-signed URL from which the file will be downloaded.
+ *
+ * @example
+ * Download a file from a pre-signed URL:
+ * downloadFile('https://example.com/path/to/file');
+ */
+export function downloadFile(presignedUrl: string): void;
+
+/**
+ * Creates and downloads a file from the provided data.
+ *
+ * @param filename - The name under which the file will be saved.
+ * @param data - The data to be downloaded, provided as BlobPart.
+ *
+ * @example
+ * Download a file from Blob data:
+ * const data = new Blob(['Hello, world!'], { type: 'text/plain' });
+ * downloadFile('example.txt', data);
+ *
+ * @example
+ * Download a file from string data:
+ * const dataString = "Sample data content";
+ * downloadFile('example.txt', dataString);
+ */
+export function downloadFile(filename: string, data: BlobPart): void;
+
+export function downloadFile(filenameOrPresignedUrl: string, data?: BlobPart): void {
+  if (isNil(data)) {
+    const anchor: HTMLAnchorElement = document.createElement('a');
+    anchor.href = filenameOrPresignedUrl;
+    anchor.click();
+    anchor.remove();
+    return;
+  }
+
+  const dataBlob: Blob = data instanceof Blob ? data : new Blob([data]);
   const anchor: HTMLAnchorElement = document.createElement('a');
   anchor.href = URL.createObjectURL(dataBlob);
-  anchor.download = filename;
+  anchor.download = filenameOrPresignedUrl;
   anchor.click();
+  URL.revokeObjectURL(anchor.href);
+  anchor.remove();
 }
