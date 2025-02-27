4c937fc37996b37867581c70a25027ee28507b10
= <=>
2025-02-27 16:25:00 +0300

    feature: extended tapOnCondition operator

--- packages/rxjs/src/operators/tap-on-condition.operator.ts
+++ packages/rxjs/src/operators/tap-on-condition.operator.ts
@@ -1,30 +1,32 @@
-import type { Observable, OperatorFunction } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { isNil } from '@bimeister/utilities.common';
+import { Observable, OperatorFunction, tap } from 'rxjs';
 
 /**
- * Conditionally applies a callback using the tap operator based on the specified condition.
+ * Conditionally applies callbacks using the tap operator based on the specified condition.
  *
  * @template T - The type of the elements emitted by the source observable.
- * @param condition - A function that determines the condition based on the emitted values.
- * @param callback - The callback function to be invoked when the condition is true.
- * @returns - An operator that performs a side effect for each emission on the source observable.
+ * @param condition - A function that determines the condition based on the emitted values or a boolean value.
+ * @param trueCallback - The callback function to be invoked when the condition is true.
+ * @param falseCallback - The optional callback function to be invoked when the condition is false.
+ * @returns - An operator that performs side effects for each emission on the source observable.
  * @example
  * // Displays a successful toast of saved settings if the value is greater than 10.
- * source$.pipe(..., tapOnCondition(value => value > 10, () => this.showSuccessToast()), ...)
+ * source$.pipe(..., tapOnCondition(value => value > 10, () => this.showSuccessToast(), () => this.showErrorToast()))
  */
 export function tapOnCondition<T>(
   condition: ((value: T) => boolean) | boolean,
-  callback: (value: T) => void
+  trueCallback: (value: T) => void,
+  falseCallback?: (value: T) => void
 ): OperatorFunction<T, T> {
   return (source$: Observable<T>) =>
     source$.pipe(
       tap((value: T) => {
-        if (typeof condition === 'function' && condition(value)) {
-          callback(value);
-        }
+        const conditionResult: boolean = typeof condition === 'function' ? condition(value) : condition;
 
-        if (typeof condition === 'boolean' && condition) {
-          callback(value);
+        if (conditionResult) {
+          trueCallback(value);
+        } else if (!isNil(falseCallback)) {
+          falseCallback(value);
         }
       })
     );
