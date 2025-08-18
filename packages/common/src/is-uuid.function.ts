0a12de098dd9faf7d8384165271b2b5eb7deb03d
Mikalai Ropat <mikalai.ropat@bimeister.com>
2025-08-18 13:42:10 +0300

    fix: BIM-66224 add isSequentialGuid validation

--- packages/common/src/is-uuid.function.ts
+++ packages/common/src/is-uuid.function.ts
@@ -2,5 +2,12 @@ import type { Uuid } from '@bimeister/utilities.types';
 import { validate } from 'uuid';
 
 export function isUuid(input: unknown): input is Uuid {
-  return typeof input === 'string' && validate(input);
+  return typeof input === 'string' && (validate(input) || isUuidLike(input));
+}
+
+/**
+ * Без проверки версии и варианта GUID
+ */
+function isUuidLike(input: string): boolean {
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(input);
 }
